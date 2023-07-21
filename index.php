8797ac6c1b04b938d57095415aacef95c59a8015
Benjamin Gutierrez <benjamingutierrez0097@gmail.com>
2023-07-20 21:58:26 -0400

    I started with the login and the interface

---
+++ index.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="login">
+        <p>Join thousands of learnes from around the world</p>
+        <p>master web development by making real-life projects. There are multiple paths for you to choose</p>
+        <input>Email
+        <input>Password 
+        <button>Start coding now</button>
+        <p>continue with these social profile</p>
+        <button>google</button>
+        <button>facebook</button>
+        <button>twitter</button>
+        <button>Github</button>
+        <p>already a member</p>   
+    </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Login</p>
+    <input>Email 
+    <input>Password 
+    <button>Login</button>
+    <p>or continue  with these social profile</p>
+</body>
+</html>
