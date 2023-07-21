c9a28772bafa7f2f34576272c18d62ed00b872bd
Benjamin Gutierrez <benjamingutierrez0097@gmail.com>
2023-07-21 08:32:30 -0400

    today I create the login of creating a account and the login with a account

--- index.php
+++ index.php
@@ -1,26 +1,36 @@
-
-
 <!DOCTYPE html>
+
+
+
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="index.css">
 </head>
+
 <body>
     <div class="login">
-        <p>Join thousands of learnes from around the world</p>
+        <p class="titleOne">Join thousands of learnes from around the world</p>
         <p>master web development by making real-life projects. There are multiple paths for you to choose</p>
-        <input>Email
-        <input>Password 
-        <button>Start coding now</button>
+        <img src="../Mini_project/public/resources/email.svg">
+        <input class="email" placeholder="Email">
+        <input class="password" placeholder="Password">
+        <button class="start">Start coding now</button>
         <p>continue with these social profile</p>
+        <div class="social">
         <button>google</button>
         <button>facebook</button>
         <button>twitter</button>
-        <button>Github</button>
-        <p>already a member</p>   
+        <button>Github</button>    
+        </div>
+        <div class="already">
+        <p>already a member?</p>
+        <button class="inicio">Login</button>
+        </div>
     </div>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -4,12 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
-    <p>Login</p>
-    <input>Email 
-    <input>Password 
-    <button>Login</button>
+    <div class="usuarioLogin">
+    <p class="titleLogin">Login</p>
+    <input class="emailLogin">Email 
+    <input class="passLogin">Password 
+    <button class="buttonLogin">Login</button>
     <p>or continue  with these social profile</p>
+    <div>
+        <button>Google</button>
+        <button>Facebook</button>
+        <button>twitter</button>
+        <button>Github</button>
+    </div>
+    </div>
 </body>
 </html>
